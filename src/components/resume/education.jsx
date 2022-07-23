a92abb8742b0b4179a1e137a806b263cadcaac28
Naveenprasanth T <102594021+NaveenprasanthT@users.noreply.github.com>
2022-07-23 13:54:26 +0530

    Update education.jsx

--- src/components/resume/education.jsx
+++ src/components/resume/education.jsx
@@ -6,7 +6,7 @@ function Education() {
             <h1>UnderGraduate Degree</h1>
             <p>BS in Computer Science,Expected in 2023 <br/> NGM College,Coimbatore.</p>
             <h1>High School</h1>
-            <p>SCMHS School, Senguttai Palayam, Pollachi<br/> Graduated in 2019</p>
+            <p>SCMHS School, Senguttai Palayam, Pollachi<br/> Graduated in 2020</p>
         </div>
      );
 }
